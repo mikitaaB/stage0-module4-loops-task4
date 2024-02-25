a9f8c85af00bee6671384e6d97a5a33f7cff0fdb
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 23:08:47 +0300

    module 4. task 4

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        for (int i = 1; i <= lastPrinted; i += 3){
+            System.out.print(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+        int result =0;
+        if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        } else if (numberToSkip < 0 && lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        } else {
+            for (int i = 0; i <= numberToSkip; i++){
+                result += i;
+            }
+            System.out.println(result);
+        }
     }
 }

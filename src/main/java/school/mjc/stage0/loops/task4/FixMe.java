9a37d6a18b52ed29e663e40dd27dcb991ebbad28
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-16 02:03:25 +0300

    module 4. task 4. fix 3

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -2,8 +2,7 @@ package school.mjc.stage0.loops.task4;
 
 public class FixMe {
     public static void main(String[] args) {
-        int numberToGoUntil = 100;
-        for (int i = 1; i <= numberToGoUntil; i++) {
+        for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             } else if (i == 7) {
